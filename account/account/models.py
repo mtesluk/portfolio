1936c6d71b84c52abae50323d0fc74f3bc0ece79
mtesluk <mateusz.tesluk@nokia.com>
2020-02-28 13:58:57 +0100

    Fix lack of Token in models account

--- account/account/models.py
+++ account/account/models.py
@@ -4,6 +4,8 @@ from django.dispatch import receiver
 from django.db.models.signals import post_save
 from django.conf import settings
 
+from rest_framework.authtoken.models import Token
+
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
