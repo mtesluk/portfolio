7df838a48ba87380e960727c567fff01e9f7d250
mtesluk <mateusz.tesluk@nokia.com>
2020-03-03 15:09:57 +0100

    Add interceptor

--- gui/src/index.tsx
+++ gui/src/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
+import axios from 'axios';
 
 import './index.scss';
 
@@ -8,6 +9,21 @@ import * as serviceWorker from './serviceWorker';
 import App from './components/App';
 import store from './store';
 
+axios.interceptors.request.use((config) => {
+  const token = localStorage['token'];
+  let headers = {...config.headers};
+  if (token) {
+    headers = {
+      ...headers,
+      'Authorization': `Token ${token}`
+    }
+    config = {
+      ...config,
+      headers
+    }
+  }
+  return config;
+});
 
 ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
 
