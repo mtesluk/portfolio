92c29d716714f4eb438403251085507995860ab1
mtesluk <mateusz@tesluk.pl>
2020-05-16 16:20:35 +0200

    Updates

---
+++ precache-manifest.889d5d7da546cfe0b8b9a1434377e158.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cdec0c098dca6f05b8409e3ea3d2d5cd",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "08eacd904bbd6e872578",
+    "url": "/portfolio/static/css/main.d99f230c.chunk.css"
+  },
+  {
+    "revision": "9a0eacd0dfbcffb82ad6",
+    "url": "/portfolio/static/js/2.fb0269e8.chunk.js"
+  },
+  {
+    "revision": "9a0dc577323b89730adaf7cf8cf465aa",
+    "url": "/portfolio/static/js/2.fb0269e8.chunk.js.LICENSE"
+  },
+  {
+    "revision": "08eacd904bbd6e872578",
+    "url": "/portfolio/static/js/main.6c490d19.chunk.js"
+  },
+  {
+    "revision": "b1e7c9b5aa53e9a1b7d1",
+    "url": "/portfolio/static/js/runtime-main.d056fcee.js"
+  },
+  {
+    "revision": "f53bee6f01f6167a31597eab90aa6588",
+    "url": "/portfolio/static/media/blog-dashboard.f53bee6f.png"
+  },
+  {
+    "revision": "986408db4bdbe02be939700f094c130e",
+    "url": "/portfolio/static/media/forum-dashboard.986408db.png"
+  },
+  {
+    "revision": "1679e80b31d18777565e6d9b62687baa",
+    "url": "/portfolio/static/media/travel-dashboard.1679e80b.jpg"
+  }
+]);
